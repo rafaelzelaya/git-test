98a245c44b377a25f36bd9f7e94580c276308811
rafaelzelaya <rafaelzelayax@gmail.com>
2020-08-02 16:01:47 -0600

    Express session part 2

--- app.js
+++ app.js
@@ -40,63 +40,37 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth (req, res, next) {
   console.log("res session ???");
   console.log(req.session);
 
   if(!req.session.user){
-    console.log("No tiene sesion abierta");
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        console.log("no estas autenticado");
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-        return;
-    }
-    console.log("Verificando tu usuario y password");
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-      //res.cookie('user','admin',{signed: true});
-      req.session.user = 'admin';
-      //res.cookie('quien','rafa estuvo aquí',{signed:true});
-      req.session.quien = 'Rafa estuvo aquí';
-      next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-    }
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
-  else {
-    console.log("Si tiene cookies");
-      if (req.session.user === 'admin') {
-        console.log('¿quien estuvo aquí? '+req.session.quien);
-        console.log('req.session: ', req.session);
-          next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 401;
-          next(err);
-      }
+  else{
+    if(req.session.user === "authenticated"){
+      next();
+    }
+    else{
+      var err = new Error("You are not authenticated!");
+      err.status = 403;
+      return next(err);
+    }
   }
 }
 
 app.use(auth);
 //
-app.use(logger('dev'));
-app.use(express.json());
+/*app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());*/
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
